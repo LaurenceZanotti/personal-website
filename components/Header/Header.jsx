421e1f1281e42955dd458c6fe64d434c95c29300
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-06-21 16:02:45 -0300

    Menu navbar is opening

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -6,20 +6,21 @@ export default function Header() {
   return (
     <header className={styles.container_header}>
       <Navbar />
-      <div className={styles.gradient_bg}>
-        <div className={styles.profile_picture}></div>
-        <div>
-          <h1 className={styles.name}>Laurence Zanotti</h1>
-          <h2 className={styles.title}>Desenvolvedor Web Front end</h2>
-        </div>
-        <p className={styles.description}>Projetando e construindo interfaces de usuário e experiências inesquecíveis</p>
-        <a href="/cv.pdf" download>
-          <div className={styles.baixar_btn}>BAIXAR CV</div>
-        </a>
-        <div className={styles.scroll_icon}>
-            <div className={styles.ball}></div>
-        </div>
+
+      <div className={styles.profile_picture}></div>
+      <div>
+        <h1 className={styles.name}>Laurence Zanotti</h1>
+        <h2 className={styles.title}>Desenvolvedor Web Front end</h2>
       </div>
+      <p className={styles.description}>Projetando e construindo interfaces de usuário e experiências inesquecíveis</p>
+      <a href="/cv.pdf" download>
+        <div className={styles.baixar_btn}>BAIXAR CV</div>
+      </a>
+      <div className={styles.scroll_icon}>
+          <div className={styles.ball}></div>
+      </div>
+      {/* <div className={styles.gradient_bg}>
+      </div> */}
     </header>
   )
 }
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,25 +1,41 @@
 import styles from './Navbar.module.css'
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+import { useState } from 'react';
 
 export default function Navbar() {
   const brazil = '/imgs/Brazil.svg'
   const usa = '/imgs/USA.svg'
 
+  const [menuActive, setMenuActive] = useState(false)
+
+  function handleClick() {
+    // Switch menu state
+    setMenuActive(prevState => !prevState)
+  }
+
   return (
-    <nav className={styles.container_navbar}>
-        <div className={styles.language_selector} id="language_selector">
-          <div className={styles.active_language}><img src={brazil} alt="" /></div>
-          <div><img src={usa} alt="" /></div>
-        </div>
-        <ul className={styles.invisible}>
-            <li>Home</li>
-            <li>Projetos</li>
-            <li>Formação</li>
-            <li>Contato</li>
-        </ul>
-        <div className={styles.hamburguer}>
-            <GiHamburgerMenu/>
-        </div>
+    <nav className={`${styles.container_navbar} ${menuActive && styles.navbar_expanded}`}>
+      <div className={styles.language_selector} id="language_selector">
+        <div className={styles.active_language}><img src={brazil} alt="Língua Português do Brasil" /></div>
+        <div><img src={usa} alt="English language" /></div>
+      </div>
+      {menuActive && 
+      <ul className={styles.menu_container}>
+          <li>Home</li>
+          <li>Projetos</li>
+          <li>Formação</li>
+          <li>Contato</li>
+      </ul>}
+      {menuActive ?
+      <div className={styles.hamburguer} onClick={handleClick}>
+        <AiOutlineCloseCircle/>
+      </div> :
+      <div className={styles.hamburguer} onClick={handleClick}>
+        <GiHamburgerMenu/>
+      </div>
+      }
     </nav>
   )
 }
