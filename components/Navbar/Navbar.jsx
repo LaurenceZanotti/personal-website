e2b86ba6faea4f984cadf96bb9faab501bd21e4b
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-10-17 01:00:20 -0300

    Navbar menu options

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -20,27 +20,34 @@ export default function Navbar() {
       ${menuActive && styles.navbar_expanded} 
       ${(firstMenuOpen && !menuActive) && styles.navbar_collapsed}
     `}>
-      {/* <div className={styles.language_selector} id="language_selector">
-        <MdLanguage/>
-      </div> */}
-      <span className='font-thin text-xl mx-2 px-2 border-r border-r-white'>
+      <span className='font-thin text-xl mx-2 px-2 border-r border-r-white whitespace-nowrap'>
         Laurence Zanotti 
       </span>
+      {/* Desktop menu options */}
+      <ul className="hidden sm:flex gap-8 font-bold">
+          <li><a className='hover:text-slate-400' href="#home">Home</a></li>
+          <li><a className='hover:text-slate-400' href="#work">Projetos</a></li>
+          <li><a className='hover:text-slate-400' href="#academic">Formação</a></li>
+          <li><a className='hover:text-slate-400' href="#contact">Contato</a></li>
+      </ul>
+      {/* Mobile hamburguer menu options */}
       {menuActive && 
       <ul className={`
         ${styles.menu_container} 
         ${menuActive && styles.menu_container_unfade}
+        sm:hidden
       `}>
           <li><a href="#home">Home</a></li>
           <li><a href="#work">Projetos</a></li>
           <li><a href="#academic">Formação</a></li>
           <li><a href="#contact">Contato</a></li>
       </ul>}
+      {/* Mobile hamburguer menu button */}
       {menuActive ?
-      <div className={styles.hamburguer} onClick={handleClick}>
+      <div className={`${styles.hamburguer} sm:invisible lg:w-[160px]`} onClick={handleClick}>
         <AiOutlineCloseCircle/>
       </div> :
-      <div className={styles.hamburguer} onClick={handleClick}>
+      <div className={`${styles.hamburguer} sm:invisible lg:w-[160px]`} onClick={handleClick}>
         <GiHamburgerMenu/>
       </div>}
     </nav>
